45365a5b01eb2e75baf7d21c73dbc5b6859cf540
gabriel <ghcosta87@gmail.com>
2021-08-23 20:18:07 -0300

    unused commands deleted some commands updated following scrips modifications

--- run_command.h
+++ run_command.h
@@ -11,16 +11,12 @@ class run_command : public QObject
     Q_OBJECT
 public:
     explicit run_command(QObject *parent=0);
-    Q_INVOKABLE void run();
+    Q_INVOKABLE void setWifiConnection();
     Q_INVOKABLE void console_fill();
     Q_INVOKABLE void conectar_dispositivo(QString);
     Q_INVOKABLE void desconectar_dispositivos();
     Q_INVOKABLE QString grabPath();
     Q_INVOKABLE void debugNetGraber(QString);
-    Q_INVOKABLE void query1(QString);
-    Q_INVOKABLE void query2(QString);
-    Q_INVOKABLE void query3(QString);
-    Q_INVOKABLE void query4(QString);
 private:
     QProcess *m_process;
 };
